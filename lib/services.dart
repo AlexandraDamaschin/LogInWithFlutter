20e3a89d2d42f85e0f81e5bc9e7252b2ae892875
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-03-13 15:16:40 +0200

    get method working- on debug mode returns json object

--- lib/services.dart
+++ lib/services.dart
@@ -2,8 +2,14 @@ import 'package:http/http.dart' as http;
 
 String url = 'http://localhost:60000/';
 
-String fetchPost() {
-  var response= http.get('https://jsonplaceholder.typicode.com/posts/1');
+Future<String> fetchPost() async{
+  final response= await http.get('https://jsonplaceholder.typicode.com/todos/1');
   print(response);
-  return response.toString();
+
+  if (response.statusCode == 200) {
+    print('RETURNING: ' + response.body);
+    return response.body;
+  } else {
+    throw Exception('Failed to load post');
+  }
 }
